9797146c18d970050f2966925d4397ff6f0f82b9
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2021-02-28 10:41:26 +0800

    update collection handle

--- memepack_builder/ModuleChecker.py
+++ memepack_builder/ModuleChecker.py
@@ -61,7 +61,7 @@ class ModuleChecker(object):
                 entry = f"Warning [{result['code']}]: {result['message']}"
                 self.__logger.append(entry)
                 self.__warning_count += 1
-        self.__module_info = module_info
+        self.__module_info = self._flatten_collection(module_info)
         self.__checked = True
 
     def _analyze_module(self, path: str):
@@ -75,24 +75,32 @@ class ModuleChecker(object):
             if data['type'] in ('resource', 'language', 'mixed'):
                 # some compatibility, planned to remove in 0.3.0
                 if data['type'] != 'resource':
-                    self._raise_warning(WARN_DEPRECATED_MODULE_TYPE, f'Module type "{data["type"]}" is deprecated, please use "resource" instead.')
+                    self._raise_warning(
+                        WARN_DEPRECATED_MODULE_CONTENT, f'In path "{dir_name}": Module type "{data["type"]}" is deprecated, please use "resource" instead.')
                     data['type'] = 'resource'
                 for func, classifier, modified_type in (self._exist_resource_dirs, MODULE_MODIFIED_RESOURCE, 'resource'), (self._exist_language_files, MODULE_MODIFIED_LANGUAGE, 'language'):
                     if func(path):
                         def add_classifier(data, classifier):
                             if 'classifier' not in data:
-                                self._raise_warning(WARN_MISSING_CLASSIFIER, f'In path "{dir_name}": Module modified {modified_type}, but cannot find corresponding classifier.')
+                                self._raise_warning(
+                                    WARN_MISSING_CLASSIFIER, f'In path "{dir_name}": Module modified {modified_type}, but cannot find corresponding classifier.')
                                 data['classifier'] = [classifier]
                             elif classifier not in data['classifier']:
-                                self._raise_warning(WARN_MISSING_CLASSIFIER, f'In path "{dir_name}": Module modified {modified_type}, but cannot find corresponding classifier.')
+                                self._raise_warning(
+                                    WARN_MISSING_CLASSIFIER, f'In path "{dir_name}": Module modified {modified_type}, but cannot find corresponding classifier.')
                                 data['classifier'].append(classifier)
                         add_classifier(data, classifier)
             elif data['type'] == 'collection':
                 if 'contains' not in data:
                     return self._result_msg(WARN_BROKEN_MODULE, f'In path "{dir_name}": Expected a module collection, but "contains" key is missing in module_manifest.json.')
                 else:
-                    if 'collection' in data['contains']:
-                        return self._result_msg(WARN_LOOP_COLLECTION, f'In path "{dir_name}": Try to contain another collection in a collection.')
+                    # compatibility start
+                    if isinstance(data['contains'], dict):
+                        self._raise_warning(
+                            WARN_DEPRECATED_MODULE_CONTENT, f'In path "{dir_name}": Deprecated "contains" structure is detected, please update it.')
+                        data['contains'] = [i for k in data['contains']
+                                            for i in data['contains'][k]]
+                    # compatibility end
             else:
                 return self._result_msg(WARN_UNKNOWN_MODULE, f'In path "{dir_name}": Unknown module type "{data["type"]}".')
             data['dirname'] = dir_name
@@ -101,7 +109,7 @@ class ModuleChecker(object):
             return self._result_msg(WARN_BROKEN_MODULE, f'In path "{dir_name}": No module_manifest.json.')
 
     def _exist_resource_dirs(self, path: str):
-        res_dir_names = 'assets', 'credits', 'models', 'textures'
+        res_dir_names = 'assets', 'credits', 'models', 'textures', 'sounds'
         for dir_name in res_dir_names:
             if os.path.exists(os.path.join(path, dir_name)) and os.path.isdir(os.path.join(path, dir_name)):
                 return True
@@ -123,3 +131,26 @@ class ModuleChecker(object):
     def _raise_warning(self, code: int, message: str):
         self.__logger.append(f'Warning [{code}]: {message}')
         self.__warning_count += 1
+
+    def _flatten_collection(self, module_info: dict):
+        resource_list = [i['name'] for i in module_info['modules']['resource']]
+        collection_list = [i['name']
+                           for i in module_info['modules']['collection']]
+        collection_info = module_info['modules']['collection']
+        for collection in collection_info:
+            flattened_names = {*collection['contains']}
+            for name in collection['contains']:
+                if name in resource_list:
+                    pass
+                elif name in collection_list:
+                    self._raise_warning(
+                        WARN_LOOP_COLLECTION, f'In path "{collection["dirname"]}": Looping collection detected.')
+                    if name in flattened_names:
+                        flattened_names.remove(name)
+                else:
+                    self._raise_warning(
+                        WARN_UNKNOWN_MODULE, f'In path "{collection["dirname"]}": Collection contains unknown module.')
+                    if name in flattened_names:
+                        flattened_names.remove(name)
+            collection['contains'] = [*flattened_names]
+        return module_info
