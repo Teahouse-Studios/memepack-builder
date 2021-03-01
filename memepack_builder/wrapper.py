a391676fc3a5e2b9480c7546371655b446777c1d
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2021-03-01 21:31:05 +0800

    update wrapper

--- memepack_builder/wrapper.py
+++ memepack_builder/wrapper.py
@@ -2,22 +2,20 @@ import sys
 from memepack_builder._internal.cli import build, generate_parser, process_args
 
 
-def _log_highlight(entry: str):
-    if entry.startswith('Warning'):
-        return f'\033[33m{entry}\033[0m', sys.stderr
-    elif entry.startswith('Error'):
-        return f'\033[1;31m{entry}\033[0m', sys.stderr
-    else:
-        return entry, sys.stdout
+def _log_highlight(log: list, highlight: bool):
+    for entry in log:
+        if entry.startswith('Warning'):
+            yield highlight and f'\033[33m{entry}\033[0m' or entry, sys.stderr
+        elif entry.startswith('Error'):
+            yield highlight and f'\033[1;31m{entry}\033[0m' or entry, sys.stderr
+        else:
+            yield entry, sys.stdout
 
 
-def main(args=None, highlight=None):
+def main(args=None, highlight=True):
     if not args:
         args = process_args(vars(generate_parser().parse_args()))
     result = build(args)
-    log = result['log']
-    if highlight:
-        log = map(_log_highlight, result['log'])
-    for entry, output in log:
+    for entry, output in _log_highlight(result['log'], highlight):
         print(entry, file=output)
     return result
