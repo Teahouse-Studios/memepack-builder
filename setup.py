463f2ffd3a966b5cb4fdc9af1a255efa2f98933f
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2021-07-31 20:32:17 +0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf8') as fh:
 
 setuptools.setup(
     name="memepack_builder",
-    version="0.2.3",
+    version="0.2.4",
     author="MysticNebula70",
     author_email="mysticnebula70@teahou.se",
     description="A tool for packaging Memefied Chinese resource pack",
